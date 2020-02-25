b87c66cc36eee222bb2d08176c1db88eec342d1b
cmcdgit <cmcdgit@gmail.com>
2020-02-25 23:21:35 +0000

    fix to be tested

--- installReGrep.sh
+++ installReGrep.sh
@@ -7,12 +7,12 @@ sudo apt install -y vim
 chmod 744 reGrep.sh
 chmod 664 folderLocation.txt
 
-echo "---------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 echo "alias reGrep='sh $HOME/.reGrep/reGrep.sh'" >> $HOME/.bashrc
-echo "export FOLDER=source $HOME/.reGrep/folderLocation.txt | grep -v '^#'" >> $HOME/.bashrc
-echo "alias setFolder='vim $HOME/.reGrep/folderLocation.txt"
+echo "export FOLDER=$(cat $HOME/.reGrep/folderLocation.txt | grep -v '^#')" >> $HOME/.bashrc
+echo "alias setFolder='vim $HOME/.reGrep/folderLocation.txt" >> $HOME/.bashrc
 
-echo "---------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 source $HOME/.bashrc
 echo "type reGrep hello (search for 'hello' using recursive-grep)"
 sleep 4
