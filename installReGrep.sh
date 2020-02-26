228bdfbf39386e565d4eba1824a398252aba8137
cmcdgit <cmcdgit@gmail.com>
2020-02-26 22:02:36 +0000

    minor changes

--- installReGrep.sh
+++ installReGrep.sh
@@ -7,7 +7,6 @@ sudo apt install -y vim
 echo "#---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 echo "alias reGrep='sh $HOME/.reGrep/reGrep.sh'" >> $HOME/.bashrc
 echo "export FOLDER=$(cat $HOME/.reGrep/folderLocation.txt | grep -v '^#')" >> $HOME/.bashrc
-echo "alias setFolder='vim $HOME/.reGrep/folderLocation.txt'" >> $HOME/.bashrc
 echo "#---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 
 echo ''
