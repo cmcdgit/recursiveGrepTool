4129033e3b1d647404b6f76ba83e1c283c8fab65
cmcdgit <cmcdgit@gmail.com>
2020-02-26 00:08:22 +0000

    small fix

--- installReGrep.sh
+++ installReGrep.sh
@@ -4,12 +4,12 @@ set -e
 sudo apt update
 sudo apt install -y vim
 
-echo "---------------------------------------------------------------------------------------" >> $HOME/.bashrc
+echo "#---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 echo "alias reGrep='sh $HOME/.reGrep/reGrep.sh'" >> $HOME/.bashrc
 echo "export FOLDER=$(cat $HOME/.reGrep/folderLocation.txt | grep -v '^#')" >> $HOME/.bashrc
 echo "alias setFolder='vim $HOME/.reGrep/folderLocation.txt'" >> $HOME/.bashrc
 
-echo "---------------------------------------------------------------------------------------" >> $HOME/.bashrc
+echo "#---------------------------------------------------------------------------------------" >> $HOME/.bashrc
 #source ~/.bashrc
 echo "close and reopen you terminal then type"
 echo "reGrep hello (search for 'hello' using recursive-grep)"
